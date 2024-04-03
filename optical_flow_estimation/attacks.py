7755f6bc5b04afc57e049158f2ad6dceead25c3c
Julian Caspary <julian.yuya.caspary@students.uni-mannheim.de>
2024-04-03 23:33:17 +0200

    adjusted cospgd to handle images of pairs individually

--- optical_flow_estimation/attacks.py
+++ optical_flow_estimation/attacks.py
@@ -42,7 +42,7 @@ from ptlflow_attacked.ptlflow.utils.utils import (
     get_list_of_available_models_list,
     tensor_dict_to_numpy,
 )
-from attack_utils.utils import get_image_tensors, get_image_grads, replace_images_dic
+from attack_utils.utils import get_image_tensors, get_image_grads, replace_images_dic, get_flow_tensors
 
 # Import cosPGD functions
 from cospgd import functions as attack_functions
@@ -565,7 +565,7 @@ def attack_one_dataloader(
                     images, labels, preds, placeholder = ffgsm(args, inputs, model)
                 case "pgd" | "cospgd":
                     # inputs["images"] = cos_pgd(args, inputs, model)
-                    images, labels, preds, losses[i] = cos_pgd(args, inputs, model)
+                    images, labels, preds, losses[i] = cos_pgd(args, inputs, model, targeted_flow_dic)
                 case "apgd":
                     # inputs["images"] = fgsm(args, inputs, model)
                     images, labels, preds, placeholder = apgd(args, inputs, model)
@@ -685,16 +685,6 @@ def fgsm(args: Namespace,inputs: Dict[str, torch.Tensor], model: BaseModel, targ
     return images, labels, preds, loss.item()
 
 
-def fgsm2(args: Namespace, inputs: Dict[str, torch.Tensor], model: BaseModel):
-    attack = FGSM(model, args.attack_epsilon)
-    perturbed_inputs = attack(inputs)
-    preds = model(perturbed_inputs)
-    images = None
-    labels = None
-
-    return images, labels, preds, None
-
-
 def ffgsm(args: Namespace, inputs: Dict[str, torch.Tensor], model: BaseModel):
     attack = FFGSM(model, args.attack_epsilon, args.attack_alpha)
     perturbed_inputs = attack(inputs)
@@ -717,7 +707,7 @@ def apgd(args: Namespace, inputs: Dict[str, torch.Tensor], model: BaseModel):
 
 
 @torch.enable_grad()
-def cos_pgd(args: Namespace, inputs: Dict[str, torch.Tensor], model: BaseModel):
+def cos_pgd(args: Namespace, inputs: Dict[str, torch.Tensor], model: BaseModel, targeted_flow_dic: Optional[Dict[str, torch.Tensor]]):
     """Perform pgd or cospgd adversarial attack on input images.
 
     Parameters
@@ -735,45 +725,62 @@ def cos_pgd(args: Namespace, inputs: Dict[str, torch.Tensor], model: BaseModel):
         Perturbed images.
     """
 
-    images = inputs["images"].squeeze(0)
-    labels = inputs["flows"].squeeze(0)
+    if args.attack_targeted:
+        labels = targeted_flow_dic["flows"].squeeze(0)
+    else:
+        labels = inputs["flows"].squeeze(0)
 
-    orig_labels = labels.clone()
-    orig_images = images.clone()
+    orig_image_1 = inputs["images"][0].clone()[0].unsqueeze(0)
+    orig_image_2 = inputs["images"][0].clone()[1].unsqueeze(0)
+
+    
     
     if args.attack_targeted:
         labels = torch.zeros_like(labels)
 
     # with torch.no_grad():
     #     orig_preds = model(images)
+    image_1, image_2 = get_image_tensors(inputs)
 
     if args.attack_norm == "inf":
-        images = attack_functions.init_linf(
-                            images,
+        image_1 = attack_functions.init_linf(
+                            image_1,
+                            epsilon = args.attack_epsilon,
+                            clamp_min = 0,
+                            clamp_max = 1
+                        )
+        image_2 = attack_functions.init_linf(
+                            image_2,
                             epsilon = args.attack_epsilon,
                             clamp_min = 0,
                             clamp_max = 1
                         )
     elif args.attack_norm == "two":
-        images = attack_functions.init_l2(
-                            images,
+        image_1 = attack_functions.init_l2(
+                            image_1,
                             epsilon = args.attack_epsilon,
                             clamp_min = 0,
                             clamp_max = 1
                         )
-          
-    images.requires_grad=True
+        image_2 = attack_functions.init_l2(
+                            image_2,
+                            epsilon = args.attack_epsilon,
+                            clamp_min = 0,
+                            clamp_max = 1
+                        )
+    perturbed_inputs = replace_images_dic(inputs, image_1, image_2)
+    perturbed_images = perturbed_inputs["images"]
+    perturbed_images.requires_grad=True
+    perturbed_inputs["images"] = perturbed_images
 
-    perturbed_inputs = inputs
-    perturbed_inputs["images"] = images.unsqueeze(0)
     preds = model(perturbed_inputs)
-    preds_raw = preds["flows"].squeeze(0)
+    pred_flows = preds["flows"].squeeze(0)
     
-    loss = criterion(preds_raw.float(), labels.float())
+    loss = criterion(pred_flows.float(), labels.float())
     for t in range(args.attack_iterations):
         if args.attack == "cospgd":
             loss = attack_functions.cospgd_scale(
-                                predictions = preds_raw,
+                                predictions = pred_flows,
                                 labels = labels.float(),
                                 loss = loss,
                                 targeted = args.attack_targeted,
@@ -781,12 +788,25 @@ def cos_pgd(args: Namespace, inputs: Dict[str, torch.Tensor], model: BaseModel):
                             )
         loss = loss.mean()
         loss.backward()
+        image_1_adv, image_2_adv = get_image_tensors(perturbed_inputs)
+        image_1_grad, image_2_grad = get_image_grads(perturbed_inputs)
         if args.attack_norm == 'inf':
-            images = attack_functions.step_inf(
-                perturbed_image = images,
+            image_1_adv = attack_functions.step_inf(
+                perturbed_image = image_1_adv,
                 epsilon = args.attack_epsilon,
-                data_grad = images.grad,
-                orig_image = orig_images,
+                data_grad = image_1_grad,
+                orig_image = orig_image_1,
+                alpha = args.attack_alpha,
+                targeted = args.attack_targeted,
+                clamp_min = 0,
+                clamp_max = 1,
+                grad_scale = None
+            )
+            image_2_adv = attack_functions.step_inf(
+                perturbed_image = image_2_adv,
+                epsilon = args.attack_epsilon,
+                data_grad = image_2_grad,
+                orig_image = orig_image_2,
                 alpha = args.attack_alpha,
                 targeted = args.attack_targeted,
                 clamp_min = 0,
@@ -794,11 +814,22 @@ def cos_pgd(args: Namespace, inputs: Dict[str, torch.Tensor], model: BaseModel):
                 grad_scale = None
             )
         elif args.attack_norm == 'two':
-            images = attack_functions.step_l2(
-                perturbed_image = images,
+            image_1_adv = attack_functions.step_l2(
+                perturbed_image = image_1_adv,
                 epsilon = args.attack_epsilon,
-                data_grad = images.grad,
-                orig_image = orig_images,
+                data_grad = image_1_grad,
+                orig_image = orig_image_1,
+                alpha = args.attack_alpha,
+                targeted = args.attack_targeted,
+                clamp_min = 0,
+                clamp_max = 1,
+                grad_scale = None
+            )
+            image_2_adv = attack_functions.step_l2(
+                perturbed_image = image_2_adv,
+                epsilon = args.attack_epsilon,
+                data_grad = image_2_grad,
+                orig_image = orig_image_2,
                 alpha = args.attack_alpha,
                 targeted = args.attack_targeted,
                 clamp_min = 0,
@@ -806,16 +837,18 @@ def cos_pgd(args: Namespace, inputs: Dict[str, torch.Tensor], model: BaseModel):
                 grad_scale = None
             )
 
-        images.requires_grad = True
-        perturbed_inputs = inputs
-        perturbed_inputs["images"] = images.unsqueeze(0)
+        perturbed_inputs = replace_images_dic(perturbed_inputs, image_1_adv, image_2_adv)
+        perturbed_images = perturbed_inputs["images"]
+        perturbed_images.requires_grad=True
+        perturbed_inputs["images"] = perturbed_images
+
         preds = model(perturbed_inputs)
-        preds_raw = preds["flows"].squeeze(0)
-        loss = criterion(preds_raw.float(), labels.float())
+        pred_flows = preds["flows"].squeeze(0)
+        loss = criterion(pred_flows.float(), labels.float())
 
     loss = loss.mean()
 
-    return images, labels, preds, loss.item()
+    return perturbed_images, labels, preds, loss.item()
 
 
 def _get_model_names(args: Namespace) -> List[str]:
