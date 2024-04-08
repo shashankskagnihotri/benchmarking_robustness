40a7c985ebbf6b226024503bf14dc95f62d0fdf6
Julian Caspary <julian.yuya.caspary@students.uni-mannheim.de>
2024-04-09 00:40:49 +0200

    updated slurm script to include apgd, bim

--- optical_flow_estimation/scripts/test_attacks.sh
+++ optical_flow_estimation/scripts/test_attacks.sh
@@ -2,7 +2,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --mem=100G
-#SBATCH --time=02:00:00
+#SBATCH --time=03:00:00
 #SBATCH --job-name=TestAttacks
 #SBATCH --output=slurm/TestAttacks.out
 #SBATCH --cpus-per-task=16
@@ -10,14 +10,17 @@
 #SBATCH --partition=gpu_4
 #SBATCH --error=slurm/TestAttacks.out
 
-echo Flownet2Kitti
+echo TestAttacks
 
 python attacks.py raft --pretrained_ckpt chairs --val_dataset chairs-val
+python attacks.py raft --pretrained_ckpt chairs --val_dataset chairs-val --attack bim
+python attacks.py raft --pretrained_ckpt chairs --val_dataset chairs-val --attack bim --attack_targeted True
 python attacks.py raft --pretrained_ckpt chairs --val_dataset chairs-val --attack fgsm
 python attacks.py raft --pretrained_ckpt chairs --val_dataset chairs-val --attack fgsm --attack_targeted True
-python attacks.py raft --pretrained_ckpt chairs --val_dataset chairs-val --attack ffgsm
-python attacks.py raft --pretrained_ckpt chairs --val_dataset chairs-val --attack ffgsm --attack_targeted True
+python attacks.py raft --pretrained_ckpt chairs --val_dataset chairs-val --attack apgd
+python attacks.py raft --pretrained_ckpt chairs --val_dataset chairs-val --attack apgd --attack_targeted True
 python attacks.py raft --pretrained_ckpt chairs --val_dataset chairs-val --attack cospgd
 python attacks.py raft --pretrained_ckpt chairs --val_dataset chairs-val --attack cospgd --attack_targeted True
 python attacks.py raft --pretrained_ckpt chairs --val_dataset chairs-val --attack pgd
 python attacks.py raft --pretrained_ckpt chairs --val_dataset chairs-val --attack pgd --attack_targeted True
+
