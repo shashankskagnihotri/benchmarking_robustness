a07658d880a5c40d1c2df5ac51852ccedf7ab0e7
Julian Caspary <julian.yuya.caspary@students.uni-mannheim.de>
2024-04-21 15:10:50 +0200

    switched args with attack_args

--- optical_flow_estimation/attacks/apgd.py
+++ optical_flow_estimation/attacks/apgd.py
@@ -9,15 +9,21 @@ from ptlflow_attacked.ptlflow.models.base_model.base_model import BaseModel
 from attacks.adversarial_attacks_pytorch.torchattacks import APGD
 # Attack parameters
 
-def apgd(args: Namespace, inputs: Dict[str, torch.Tensor], model: BaseModel, targeted_inputs: Optional[Dict[str, torch.Tensor]]):
-    if args.attack_targeted:
+def apgd(attack_args: Dict[str, List[object]], inputs: Dict[str, torch.Tensor], model: BaseModel, targeted_inputs: Optional[Dict[str, torch.Tensor]]):
+    norm = attack_args["attack_norm"]
+    if norm == "inf":
+        norm = "Linf"
+    elif norm == "two":
+        norm = "L2"
+    if attack_args["attack_targeted"]:
         # TODO: are iterations the same steps?
-        attack = APGD(model, eps=args.attack_epsilon, loss=args.attack_loss, verbose=False, steps=args.attack_iterations)
+        # TODO: implement other iterations
+        attack = APGD(model, eps=attack_args["attack_epsilon"], loss=attack_args["attack_loss"], verbose=False, steps=attack_args["attack_iterations"])
         attack.targeted = True
         attack.set_mode_targeted_by_label()
         perturbed_images = attack(inputs["images"], targeted_inputs["flows"])
     else:
-        attack = APGD(model, eps=args.attack_epsilon, loss=args.attack_loss, verbose=False, steps=args.attack_iterations)
+        attack = APGD(model, eps=attack_args["attack_epsilon"], loss=attack_args["attack_loss"], verbose=False, steps=attack_args["attack_iterations"])
         perturbed_images = attack(inputs["images"], inputs["flows"])
     perturbed_inputs = inputs
     perturbed_inputs["images"] = perturbed_images
--- optical_flow_estimation/attacks/bim_pgd_cospgd.py
+++ optical_flow_estimation/attacks/bim_pgd_cospgd.py
@@ -11,7 +11,7 @@ import torch.nn as nn
 from attacks.attack_utils.loss_criterion import LossCriterion
 
 @torch.enable_grad()
-def bim_pgd_cospgd(args: Namespace, inputs: Dict[str, torch.Tensor], model: BaseModel, targeted_inputs: Optional[Dict[str, torch.Tensor]]):
+def bim_pgd_cospgd(attack_args: Dict[str, List[object]], inputs: Dict[str, torch.Tensor], model: BaseModel, targeted_inputs: Optional[Dict[str, torch.Tensor]]):
     """Perform bim, pgd or cospgd adversarial attack on input images.
 
     Parameters
@@ -29,47 +29,47 @@ def bim_pgd_cospgd(args: Namespace, inputs: Dict[str, torch.Tensor], model: Base
         Perturbed images.
     """
 
-    if args.attack_targeted:
+    if attack_args["attack_targeted"]:
         labels = targeted_inputs["flows"].squeeze(0)
     else:
         labels = inputs["flows"].squeeze(0)
 
-    criterion = LossCriterion(args.attack_loss)
+    criterion = LossCriterion(attack_args["attack_loss"])
 
     orig_image_1 = inputs["images"][0].clone()[0].unsqueeze(0)
     orig_image_2 = inputs["images"][0].clone()[1].unsqueeze(0)
 
     image_1, image_2 = get_image_tensors(inputs)
     
-    if 'pgd' in args.attack:
-        if args.attack_norm == "inf":
+    if 'pgd' in attack_args["attack"]:
+        if attack_args["attack_norm"] == "inf":
             image_1 = attack_functions.init_linf(
                                 image_1,
-                                epsilon = args.attack_epsilon,
+                                epsilon = attack_args["attack_epsilon"],
                                 clamp_min = 0,
                                 clamp_max = 1
                             )
             image_2 = attack_functions.init_linf(
                                 image_2,
-                                epsilon = args.attack_epsilon,
+                                epsilon = attack_args["attack_epsilon"],
                                 clamp_min = 0,
                                 clamp_max = 1
                             )
-        elif args.attack_norm == "two":
+        elif attack_args["attack_norm"] == "two":
             image_1 = attack_functions.init_l2(
                                 image_1,
-                                epsilon = args.attack_epsilon,
+                                epsilon = attack_args["attack_epsilon"],
                                 clamp_min = 0,
                                 clamp_max = 1
                             )
             image_2 = attack_functions.init_l2(
                                 image_2,
-                                epsilon = args.attack_epsilon,
+                                epsilon = attack_args["attack_epsilon"],
                                 clamp_min = 0,
                                 clamp_max = 1
                             )
     else:
-        args.attack_alpha = args.attack_epsilon
+        attack_args["attack_alpha"] = attack_args["attack_epsilon"]
     
     perturbed_inputs = replace_images_dic(inputs, image_1, image_2)
     perturbed_images = perturbed_inputs["images"]
@@ -81,61 +81,61 @@ def bim_pgd_cospgd(args: Namespace, inputs: Dict[str, torch.Tensor], model: Base
     
     # loss = criterion(pred_flows.float(), labels.float())
     loss = criterion.loss(pred_flows.float(), labels.float())
-    for t in range(args.attack_iterations):
-        if args.attack == "cospgd":
+    for t in range(attack_args["attack_iterations"]):
+        if attack_args["attack"] == "cospgd":
             loss = attack_functions.cospgd_scale(
                                 predictions = pred_flows,
                                 labels = labels.float(),
                                 loss = loss,
-                                targeted = args.attack_targeted,
+                                targeted = attack_args["attack_targeted"],
                                 one_hot = False
                             )
         loss = loss.mean()
         loss.backward()
         image_1_adv, image_2_adv = get_image_tensors(perturbed_inputs)
         image_1_grad, image_2_grad = get_image_grads(perturbed_inputs)
-        if args.attack_norm == 'inf':
+        if attack_args["attack_norm"] == 'inf':
             image_1_adv = attack_functions.step_inf(
                 perturbed_image = image_1_adv,
-                epsilon = args.attack_epsilon,
+                epsilon = attack_args["attack_epsilon"],
                 data_grad = image_1_grad,
                 orig_image = orig_image_1,
-                alpha = args.attack_alpha,
-                targeted = args.attack_targeted,
+                alpha = attack_args["attack_alpha"],
+                targeted = attack_args["attack_targeted"],
                 clamp_min = 0,
                 clamp_max = 1,
                 grad_scale = None
             )
             image_2_adv = attack_functions.step_inf(
                 perturbed_image = image_2_adv,
-                epsilon = args.attack_epsilon,
+                epsilon = attack_args["attack_epsilon"],
                 data_grad = image_2_grad,
                 orig_image = orig_image_2,
-                alpha = args.attack_alpha,
-                targeted = args.attack_targeted,
+                alpha = attack_args["attack_alpha"],
+                targeted = attack_args["attack_targeted"],
                 clamp_min = 0,
                 clamp_max = 1,
                 grad_scale = None
             )
-        elif args.attack_norm == 'two':
+        elif attack_args["attack_norm"] == 'two':
             image_1_adv = attack_functions.step_l2(
                 perturbed_image = image_1_adv,
-                epsilon = args.attack_epsilon,
+                epsilon = attack_args["attack_epsilon"],
                 data_grad = image_1_grad,
                 orig_image = orig_image_1,
-                alpha = args.attack_alpha,
-                targeted = args.attack_targeted,
+                alpha = attack_args["attack_alpha"],
+                targeted = attack_args["attack_targeted"],
                 clamp_min = 0,
                 clamp_max = 1,
                 grad_scale = None
             )
             image_2_adv = attack_functions.step_l2(
                 perturbed_image = image_2_adv,
-                epsilon = args.attack_epsilon,
+                epsilon = attack_args["attack_epsilon"],
                 data_grad = image_2_grad,
                 orig_image = orig_image_2,
-                alpha = args.attack_alpha,
-                targeted = args.attack_targeted,
+                alpha = attack_args["attack_alpha"],
+                targeted = attack_args["attack_targeted"],
                 clamp_min = 0,
                 clamp_max = 1,
                 grad_scale = None
