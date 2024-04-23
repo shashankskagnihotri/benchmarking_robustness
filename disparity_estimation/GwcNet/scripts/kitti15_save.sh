daddeb907d420e055098d2275037688445c21c75
adackerm@uni-mannheim.de <ma_adackerm@uc2n995.localdomain>
2024-04-23 16:20:21 +0200

    changes to the datapath of kitti

--- disparity_estimation/GwcNet/scripts/kitti15_save.sh
+++ disparity_estimation/GwcNet/scripts/kitti15_save.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -x
-DATAPATH="/home/xyguo/data/kitti_2015/"
+DATAPATH="/pfs/work7/workspace/scratch/ma_adackerm-team_project_fss2024/dataset/KITTI_2015/"
 python save_disp.py --datapath $DATAPATH --testlist ./filenames/kitti15_test.txt --model gwcnet-g --loadckpt ./checkpoints/kitti15/gwcnet-g/best.ckpt
