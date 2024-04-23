06572c9920b8183044c511f51df3abad7c7ec3e5
adackerm@uni-mannheim.de <ma_adackerm@uc2n995.localdomain>
2024-04-23 16:06:30 +0200

    Added GWCNET (and already made changes to run Net with kitti dataset)

---
+++ disparity_estimation/GwcNet/scripts/kitti15.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -x
+
+# Change directory to the directory containing main.py
+cd /pfs/work7/workspace/scratch/ma_adackerm-team_project_fss2024/benchmarking_robustness/disparity_estimation/GwcNet/
+
+DATAPATH="/pfs/work7/workspace/scratch/ma_adackerm-team_project_fss2024/dataset/KITTI_2015/"
+python -W ignore main.py --dataset kitti \
+    --datapath $DATAPATH --trainlist ./filenames/kitti15_train.txt --testlist ./filenames/kitti15_val.txt \
+    --epochs 300 --lrepochs "200:10" \
+    --model gwcnet-g --logdir ./checkpoints/kitti15/gwcnet-g --loadckpt ./checkpoints/kitti15/gwcnet-g/best.ckpt \
+    --test_batch_size 1
