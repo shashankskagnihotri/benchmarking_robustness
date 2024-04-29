ce1d63722b37c78e760f8e029fd6afd43d294907
xinyyan <74373346+xinyyan@users.noreply.github.com>
2024-04-29 14:12:24 +0200

    test_attacks.sh

--- optical_flow_estimation/scripts/test_attacks.sh
+++ optical_flow_estimation/scripts/test_attacks.sh
@@ -24,3 +24,5 @@ python attacks.py raft --pretrained_ckpt chairs --val_dataset chairs-val --attac
 python attacks.py raft --pretrained_ckpt chairs --val_dataset chairs-val --attack pgd
 python attacks.py raft --pretrained_ckpt chairs --val_dataset chairs-val --attack pgd --attack_targeted True
 
+#snow
+python attacks.py gma --pretrained_ckpt sintel --val_dataset sintel-val --attack weather --weather_steps 750  --weather_learn_offset True --weather_learn_motionoffset True --weather_learn_color True --weather_learn_transparency True --weather_alph_motion 1000. --weather_alph_motionoffset 1000. --weather_data /pfs/work7/workspace/scratch/ma_xinygao-team_project_fss2024/benchmarking_robustness/optical_flow_estimation/DistractingDownpour/particles_3000_npz --weather_dataset Sintel --weather_dataset_stage training --attack_targeted True --weather_depth_check True
