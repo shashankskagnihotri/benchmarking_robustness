94a1ab785c21aa4c74cf410d7f5110707e3b81be
roesch01 <fabi.roesch01@gmail.com>
2024-05-11 11:20:33 +0200

    ETH3D dataloader implemented

--- disparity_estimation/CFNet/scripts/eth3d_save.sh
+++ disparity_estimation/CFNet/scripts/eth3d_save.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 set -x
-DATAPATH="/home1/datasets/Database/robust/kitti12_15/"
-CUDA_VISIBLE_DEVICES=2 python save_disp_eth3d.py --datapath $DATAPATH --testlist ./filenames/kitti15_test.txt --model cfnet --maxdisp 256 \
---loadckpt "/home3/raozhibo/jack/shenzhelun/unet_confidence_test/down/checkpoints/robust_pretrain55/300_100_final50/best_upload"
+DATAPATH="/pfs/work7/workspace/scratch/ma_faroesch-team_project_fss2024/dataset/eth3d/"
+python save_disp_eth3d.py \
+    --datapath $DATAPATH \
+    --testlist ./filenames/kitti15_test.txt \
+    --model cfnet \
+    --dataset eth3d \
+    --maxdisp 256 \
+    --loadckpt "/pfs/work7/workspace/scratch/ma_faroesch-team_project_fss2024/pretrained_weights/cfnet/finetuning_model.ckpt"
