0ec076eb9fa96afdd4dbb20af8303bb85dcba8a7
David Schader <da.schader@gmail.com>
2024-05-16 13:58:57 +0200

    Training of Mask2Former Swin B ADE20K didn't finish because the slurm job ran out of time. Now resuming training.

--- semantic_segmentation/scripts/train_mask2former_swin-b-in1k-384x384-pre_8xb2-160k_ade20k-512x512.sh
+++ semantic_segmentation/scripts/train_mask2former_swin-b-in1k-384x384-pre_8xb2-160k_ade20k-512x512.sh
@@ -6,8 +6,8 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=100G
 #SBATCH --cpus-per-task=16
-#SBATCH --job-name=train_mask2former_swin-b-in1k-384x384-pre_8xb2-160k_ade20k-512x512
-#SBATCH --output=slurm/train_mask2former_swin-b-in1k-384x384-pre_8xb2-160k_ade20k-512x512.out
+#SBATCH --job-name=train_mask2former_swin-b-in1k-384x384-pre_8xb2-160k_ade20k-512x512_resume
+#SBATCH --output=slurm/train_mask2former_swin-b-in1k-384x384-pre_8xb2-160k_ade20k-512x512_resume.out
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=david.schader@students.uni-mannheim.de
 
@@ -15,7 +15,7 @@ echo "Started at $(date)";
 
 cd mmsegmentation
 
-python tools/train.py ../configs/mask2former/mask2former_swin-b-in1k-384x384-pre_8xb2-160k_ade20k-512x512.py --work-dir ../work_dirs/mask2former_swin-b-in1k-384x384-pre_8xb2-160k_ade20k-512x512
+python tools/train.py ../configs/mask2former/mask2former_swin-b-in1k-384x384-pre_8xb2-160k_ade20k-512x512.py --work-dir ../work_dirs/mask2former_swin-b-in1k-384x384-pre_8xb2-160k_ade20k-512x512 --resume
 
 end='date +%s'
 runtime=$((end-start))
