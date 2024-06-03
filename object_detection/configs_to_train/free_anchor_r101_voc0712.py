838022139414a07a3cfd16ee9a87d856a7e2e148
Ruben Weber <ruben.weber@students.uni-mannheim.de>
2024-06-03 16:44:40 +0200

    Revert "delted all prefix voc logfiles and restarted config_verifier; Found out, that we used the old implementation of detic which had no loss

---
+++ object_detection/configs_to_train/free_anchor_r101_voc0712.py
@@ -0,0 +1,689 @@
+auto_scale_lr = dict(base_batch_size=16, enable=True)
+backend_args = None
+data_root = 'data/VOCdevkit/'
+dataset_type = 'CocoDataset'
+default_hooks = dict(
+    checkpoint=dict(interval=1, type='CheckpointHook'),
+    logger=dict(interval=50, type='LoggerHook'),
+    param_scheduler=dict(type='ParamSchedulerHook'),
+    sampler_seed=dict(type='DistSamplerSeedHook'),
+    timer=dict(type='IterTimerHook'),
+    visualization=dict(type='DetVisualizationHook'))
+default_scope = 'mmdet'
+env_cfg = dict(
+    cudnn_benchmark=False,
+    dist_cfg=dict(backend='nccl'),
+    mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0))
+img_scales = [
+    (
+        1333,
+        800,
+    ),
+    (
+        666,
+        400,
+    ),
+    (
+        2000,
+        1200,
+    ),
+]
+load_from = None
+log_level = 'INFO'
+log_processor = dict(by_epoch=True, type='LogProcessor', window_size=50)
+model = dict(
+    backbone=dict(
+        depth=101,
+        frozen_stages=1,
+        init_cfg=dict(checkpoint='torchvision://resnet101', type='Pretrained'),
+        norm_cfg=dict(requires_grad=True, type='BN'),
+        norm_eval=True,
+        num_stages=4,
+        out_indices=[
+            0,
+            1,
+            2,
+            3,
+        ],
+        style='pytorch',
+        type='ResNet'),
+    bbox_head=dict(
+        anchor_generator=dict(
+            octave_base_scale=4,
+            ratios=[
+                0.5,
+                1.0,
+                2.0,
+            ],
+            scales_per_octave=3,
+            strides=[
+                8,
+                16,
+                32,
+                64,
+                128,
+            ],
+            type='AnchorGenerator'),
+        bbox_coder=dict(
+            target_means=[
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+            ],
+            target_stds=[
+                0.1,
+                0.1,
+                0.2,
+                0.2,
+            ],
+            type='DeltaXYWHBBoxCoder'),
+        feat_channels=256,
+        in_channels=256,
+        loss_bbox=dict(beta=0.11, loss_weight=0.75, type='SmoothL1Loss'),
+        num_classes=20,
+        stacked_convs=4,
+        type='FreeAnchorRetinaHead'),
+    data_preprocessor=dict(
+        bgr_to_rgb=True,
+        mean=[
+            123.675,
+            116.28,
+            103.53,
+        ],
+        pad_size_divisor=32,
+        std=[
+            58.395,
+            57.12,
+            57.375,
+        ],
+        type='DetDataPreprocessor'),
+    neck=dict(
+        add_extra_convs='on_input',
+        in_channels=[
+            256,
+            512,
+            1024,
+            2048,
+        ],
+        num_outs=5,
+        out_channels=256,
+        start_level=1,
+        type='FPN'),
+    test_cfg=dict(
+        max_per_img=100,
+        min_bbox_size=0,
+        nms=dict(iou_threshold=0.5, type='nms'),
+        nms_pre=1000,
+        score_thr=0.05),
+    train_cfg=dict(
+        allowed_border=-1,
+        assigner=dict(
+            ignore_iof_thr=-1,
+            min_pos_iou=0,
+            neg_iou_thr=0.4,
+            pos_iou_thr=0.5,
+            type='MaxIoUAssigner'),
+        debug=False,
+        pos_weight=-1,
+        sampler=dict(type='PseudoSampler')),
+    type='RetinaNet')
+optim_wrapper = dict(
+    clip_grad=dict(max_norm=35, norm_type=2),
+    optimizer=dict(lr=0.01, momentum=0.9, type='SGD', weight_decay=0.0001),
+    type='OptimWrapper')
+param_scheduler = [
+    dict(
+        begin=0, by_epoch=False, end=166, start_factor=0.001, type='LinearLR'),
+    dict(
+        begin=0,
+        by_epoch=True,
+        end=4,
+        gamma=0.1,
+        milestones=[
+            2,
+            3,
+        ],
+        type='MultiStepLR'),
+]
+resume = False
+test_cfg = dict(type='TestLoop')
+test_dataloader = dict(
+    batch_size=1,
+    dataset=dict(
+        ann_file='voc_coco_fmt_annotations/voc07_test.json',
+        data_prefix=dict(img=''),
+        data_root='data/VOCdevkit/',
+        metainfo=dict(
+            classes=(
+                'aeroplane',
+                'bicycle',
+                'bird',
+                'boat',
+                'bottle',
+                'bus',
+                'car',
+                'cat',
+                'chair',
+                'cow',
+                'diningtable',
+                'dog',
+                'horse',
+                'motorbike',
+                'person',
+                'pottedplant',
+                'sheep',
+                'sofa',
+                'train',
+                'tvmonitor',
+            ),
+            palette=[
+                (
+                    106,
+                    0,
+                    228,
+                ),
+                (
+                    119,
+                    11,
+                    32,
+                ),
+                (
+                    165,
+                    42,
+                    42,
+                ),
+                (
+                    0,
+                    0,
+                    192,
+                ),
+                (
+                    197,
+                    226,
+                    255,
+                ),
+                (
+                    0,
+                    60,
+                    100,
+                ),
+                (
+                    0,
+                    0,
+                    142,
+                ),
+                (
+                    255,
+                    77,
+                    255,
+                ),
+                (
+                    153,
+                    69,
+                    1,
+                ),
+                (
+                    120,
+                    166,
+                    157,
+                ),
+                (
+                    0,
+                    182,
+                    199,
+                ),
+                (
+                    0,
+                    226,
+                    252,
+                ),
+                (
+                    182,
+                    182,
+                    255,
+                ),
+                (
+                    0,
+                    0,
+                    230,
+                ),
+                (
+                    220,
+                    20,
+                    60,
+                ),
+                (
+                    163,
+                    255,
+                    0,
+                ),
+                (
+                    0,
+                    82,
+                    0,
+                ),
+                (
+                    3,
+                    95,
+                    161,
+                ),
+                (
+                    0,
+                    80,
+                    100,
+                ),
+                (
+                    183,
+                    130,
+                    88,
+                ),
+            ]),
+        pipeline=[
+            dict(backend_args=None, type='LoadImageFromFile'),
+            dict(keep_ratio=True, scale=(
+                1000,
+                600,
+            ), type='Resize'),
+            dict(type='LoadAnnotations', with_bbox=True),
+            dict(
+                meta_keys=(
+                    'img_id',
+                    'img_path',
+                    'ori_shape',
+                    'img_shape',
+                    'scale_factor',
+                ),
+                type='PackDetInputs'),
+        ],
+        type='CocoDataset'))
+test_evaluator = dict(
+    ann_file='data/VOCdevkit/voc_coco_fmt_annotations/voc07_test.json',
+    backend_args=None,
+    format_only=False,
+    metric='bbox',
+    type='CocoMetric')
+test_pipeline = [
+    dict(backend_args=None, type='LoadImageFromFile'),
+    dict(keep_ratio=True, scale=(
+        1000,
+        600,
+    ), type='Resize'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        meta_keys=(
+            'img_id',
+            'img_path',
+            'ori_shape',
+            'img_shape',
+            'scale_factor',
+        ),
+        type='PackDetInputs'),
+]
+train_cfg = dict(max_epochs=4, type='EpochBasedTrainLoop', val_interval=1)
+train_dataloader = dict(
+    batch_size=2,
+    dataset=dict(
+        dataset=dict(
+            ann_file='voc_coco_fmt_annotations/voc0712_trainval.json',
+            backend_args=None,
+            data_prefix=dict(img=''),
+            data_root='data/VOCdevkit/',
+            filter_cfg=dict(filter_empty_gt=True, min_size=32),
+            metainfo=dict(
+                classes=(
+                    'aeroplane',
+                    'bicycle',
+                    'bird',
+                    'boat',
+                    'bottle',
+                    'bus',
+                    'car',
+                    'cat',
+                    'chair',
+                    'cow',
+                    'diningtable',
+                    'dog',
+                    'horse',
+                    'motorbike',
+                    'person',
+                    'pottedplant',
+                    'sheep',
+                    'sofa',
+                    'train',
+                    'tvmonitor',
+                ),
+                palette=[
+                    (
+                        106,
+                        0,
+                        228,
+                    ),
+                    (
+                        119,
+                        11,
+                        32,
+                    ),
+                    (
+                        165,
+                        42,
+                        42,
+                    ),
+                    (
+                        0,
+                        0,
+                        192,
+                    ),
+                    (
+                        197,
+                        226,
+                        255,
+                    ),
+                    (
+                        0,
+                        60,
+                        100,
+                    ),
+                    (
+                        0,
+                        0,
+                        142,
+                    ),
+                    (
+                        255,
+                        77,
+                        255,
+                    ),
+                    (
+                        153,
+                        69,
+                        1,
+                    ),
+                    (
+                        120,
+                        166,
+                        157,
+                    ),
+                    (
+                        0,
+                        182,
+                        199,
+                    ),
+                    (
+                        0,
+                        226,
+                        252,
+                    ),
+                    (
+                        182,
+                        182,
+                        255,
+                    ),
+                    (
+                        0,
+                        0,
+                        230,
+                    ),
+                    (
+                        220,
+                        20,
+                        60,
+                    ),
+                    (
+                        163,
+                        255,
+                        0,
+                    ),
+                    (
+                        0,
+                        82,
+                        0,
+                    ),
+                    (
+                        3,
+                        95,
+                        161,
+                    ),
+                    (
+                        0,
+                        80,
+                        100,
+                    ),
+                    (
+                        183,
+                        130,
+                        88,
+                    ),
+                ]),
+            pipeline=[
+                dict(backend_args=None, type='LoadImageFromFile'),
+                dict(type='LoadAnnotations', with_bbox=True),
+                dict(keep_ratio=True, scale=(
+                    1000,
+                    600,
+                ), type='Resize'),
+                dict(prob=0.5, type='RandomFlip'),
+                dict(type='PackDetInputs'),
+            ],
+            type='CocoDataset'),
+        times=3,
+        type='RepeatDataset'))
+train_pipeline = [
+    dict(backend_args=None, type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(keep_ratio=True, scale=(
+        1000,
+        600,
+    ), type='Resize'),
+    dict(prob=0.5, type='RandomFlip'),
+    dict(type='PackDetInputs'),
+]
+tta_model = dict(
+    tta_cfg=dict(max_per_img=100, nms=dict(iou_threshold=0.5, type='nms')),
+    type='DetTTAModel')
+tta_pipeline = [
+    dict(backend_args=None, type='LoadImageFromFile'),
+    dict(
+        transforms=[
+            [
+                dict(keep_ratio=True, scale=(
+                    1333,
+                    800,
+                ), type='Resize'),
+                dict(keep_ratio=True, scale=(
+                    666,
+                    400,
+                ), type='Resize'),
+                dict(keep_ratio=True, scale=(
+                    2000,
+                    1200,
+                ), type='Resize'),
+            ],
+            [
+                dict(prob=1.0, type='RandomFlip'),
+                dict(prob=0.0, type='RandomFlip'),
+            ],
+            [
+                dict(type='LoadAnnotations', with_bbox=True),
+            ],
+            [
+                dict(
+                    meta_keys=(
+                        'img_id',
+                        'img_path',
+                        'ori_shape',
+                        'img_shape',
+                        'scale_factor',
+                        'flip',
+                        'flip_direction',
+                    ),
+                    type='PackDetInputs'),
+            ],
+        ],
+        type='TestTimeAug'),
+]
+val_cfg = dict(type='ValLoop')
+val_dataloader = dict(
+    batch_size=1,
+    dataset=dict(
+        ann_file='voc_coco_fmt_annotations/voc07_test.json',
+        data_prefix=dict(img=''),
+        data_root='data/VOCdevkit/',
+        metainfo=dict(
+            classes=(
+                'aeroplane',
+                'bicycle',
+                'bird',
+                'boat',
+                'bottle',
+                'bus',
+                'car',
+                'cat',
+                'chair',
+                'cow',
+                'diningtable',
+                'dog',
+                'horse',
+                'motorbike',
+                'person',
+                'pottedplant',
+                'sheep',
+                'sofa',
+                'train',
+                'tvmonitor',
+            ),
+            palette=[
+                (
+                    106,
+                    0,
+                    228,
+                ),
+                (
+                    119,
+                    11,
+                    32,
+                ),
+                (
+                    165,
+                    42,
+                    42,
+                ),
+                (
+                    0,
+                    0,
+                    192,
+                ),
+                (
+                    197,
+                    226,
+                    255,
+                ),
+                (
+                    0,
+                    60,
+                    100,
+                ),
+                (
+                    0,
+                    0,
+                    142,
+                ),
+                (
+                    255,
+                    77,
+                    255,
+                ),
+                (
+                    153,
+                    69,
+                    1,
+                ),
+                (
+                    120,
+                    166,
+                    157,
+                ),
+                (
+                    0,
+                    182,
+                    199,
+                ),
+                (
+                    0,
+                    226,
+                    252,
+                ),
+                (
+                    182,
+                    182,
+                    255,
+                ),
+                (
+                    0,
+                    0,
+                    230,
+                ),
+                (
+                    220,
+                    20,
+                    60,
+                ),
+                (
+                    163,
+                    255,
+                    0,
+                ),
+                (
+                    0,
+                    82,
+                    0,
+                ),
+                (
+                    3,
+                    95,
+                    161,
+                ),
+                (
+                    0,
+                    80,
+                    100,
+                ),
+                (
+                    183,
+                    130,
+                    88,
+                ),
+            ]),
+        pipeline=[
+            dict(backend_args=None, type='LoadImageFromFile'),
+            dict(keep_ratio=True, scale=(
+                1000,
+                600,
+            ), type='Resize'),
+            dict(type='LoadAnnotations', with_bbox=True),
+            dict(
+                meta_keys=(
+                    'img_id',
+                    'img_path',
+                    'ori_shape',
+                    'img_shape',
+                    'scale_factor',
+                ),
+                type='PackDetInputs'),
+        ],
+        type='CocoDataset'))
+val_evaluator = dict(
+    ann_file='data/VOCdevkit/voc_coco_fmt_annotations/voc07_test.json',
+    backend_args=None,
+    format_only=False,
+    metric='bbox',
+    type='CocoMetric')
+vis_backends = [
+    dict(type='LocalVisBackend'),
+]
+visualizer = dict(
+    name='visualizer',
+    type='DetLocalVisualizer',
+    vis_backends=[
+        dict(type='LocalVisBackend'),
+    ])
