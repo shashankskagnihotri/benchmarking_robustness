7e71daa4d9b92b6cf19fdd495b88e1aa9649fc22
LucaLuca1124 <schwarz.luca1@gmx.de>
2024-06-17 15:22:49 +0200

    feat: flowformer test script

--- optical_flow_estimation/scripts/evaluation_minimum/FlowFormer/kitti-2015/test.sh
+++ optical_flow_estimation/scripts/evaluation_minimum/FlowFormer/kitti-2015/test.sh
@@ -5,10 +5,10 @@
 #SBATCH --time=00:29:59
 #SBATCH --cpus-per-task=16
 #SBATCH --gres=gpu:1
-#SBATCH --partition=gpu_4_h100
+#SBATCH --partition=gpu_4_a100
 #SBATCH --job-name=test
 #SBATCH --output=slurm/test.out
-#SBATCH --error=slurm/test.out
+#SBATCH --error=slurm/test_err.out
 
 cd ../../../../
 python attacks.py flowformer --pretrained_ckpt kitti --val_dataset kitti-2015
