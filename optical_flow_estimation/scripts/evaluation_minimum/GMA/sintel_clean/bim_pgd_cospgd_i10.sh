a1e5e6983c21a0da385b8165501e22436637ff86
Julian Caspary <julian.yuya.caspary@students.uni-mannheim.de>
2024-06-24 12:11:39 +0200

    metrics scripts updated

---
+++ optical_flow_estimation/scripts/evaluation_minimum/GMA/sintel_clean/bim_pgd_cospgd_i10.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=100G
+#SBATCH --time=00:29:59
+#SBATCH --cpus-per-task=16
+#SBATCH --gres=gpu:1
+#SBATCH --partition=gpu_4
+#SBATCH --array=0-44%4
+#SBATCH --job-name=gma_sintel-clean_bim_pgd_cospgd_i10
+#SBATCH --output=slurm/gma_sintel-clean_bim_pgd_cospgd_i10_%A_%a.out
+#SBATCH --error=slurm/gma_sintel-clean_bim_pgd_cospgd_i10_err_%A_%a.out
+
+model="gma"
+dataset="sintel-clean"
+checkpoint="sintel"
+targeteds="True False"
+targets="negative zero"
+norms="inf two"
+attacks="bim pgd cospgd"
+iterations="10"
+jobnum=0
+#SLURM_ARRAY_TASK_ID=0
+
+cd ../../../../
+
+for norm in $norms
+do
+    if [[ $norm = "inf" ]]
+    then
+        epsilons="1 2 4 8"
+        alphas="0.01"          
+    else
+        epsilons="0.005"
+        alphas="0.0000001"
+    fi
+    for epsilon in $epsilons
+    do
+        if [[ $norm = "inf" ]]
+        then
+            epsilon=$(echo "scale=10; $epsilon/255" | bc)
+        fi
+        for alpha in $alphas
+        do
+            for attack in $attacks
+            do
+                for iteration in $iterations
+                do
+                    for targeted in $targeteds
+                    do
+                        if [[ $targeted = "True" ]]
+                        then
+                            for target in $targets
+                            do      
+                                if [[ $SLURM_ARRAY_TASK_ID -eq $jobnum ]]
+                                then
+                                    echo "Running job $model $checkpoint $dataset $attack $iteration $norm $alpha $epsilon $targeted $target $jobnum"
+                                    python attacks.py \
+                                        $model \
+                                        --pretrained_ckpt $checkpoint \
+                                        --val_dataset $dataset \
+                                        --attack $attack \
+                                        --attack_iterations $iteration \
+                                        --attack_norm $norm \
+                                        --attack_alpha $alpha \
+                                        --attack_epsilon $epsilon \
+                                        --attack_targeted $targeted \
+                                        --attack_target $target \
+                                        --write_outputs                                       
+                                    #SLURM_ARRAY_TASK_ID=$((SLURM_ARRAY_TASK_ID + 1))
+                                fi
+                                jobnum=$((jobnum + 1))
+                            done
+                        else                           
+                            if [[ $SLURM_ARRAY_TASK_ID -eq $jobnum ]]
+                            then
+                                echo "Running job $model $checkpoint $dataset $attack $iteration $norm $alpha $epsilon $targeted $target $jobnum"
+                                python attacks.py \
+                                    $model \
+                                    --pretrained_ckpt $checkpoint \
+                                    --val_dataset $dataset \
+                                    --attack $attack \
+                                    --attack_iterations $iteration \
+                                    --attack_norm $norm \
+                                    --attack_alpha $alpha \
+                                    --attack_epsilon $epsilon \
+                                    --attack_targeted $targeted \
+                                    --attack_target "zero" \
+                                    --write_outputs 
+                                #SLURM_ARRAY_TASK_ID=$((SLURM_ARRAY_TASK_ID + 1))
+                            fi
+                            jobnum=$((jobnum + 1))
+                        fi
+                    done
+                done
+            done
+        done
+    done
+done
