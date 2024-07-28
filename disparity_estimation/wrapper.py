3616f3005753c43c8bfd9278dc9b877bcf5f1f8f
roesch01 <fabi.roesch01@gmail.com>
2024-07-28 13:20:09 +0200

    Updates wrapper file

--- disparity_estimation/wrapper.py
+++ disparity_estimation/wrapper.py
@@ -17,11 +17,21 @@ if args.scenario == "commoncorruption" and (args.commoncorruption is None or arg
     raise ValueError("If --mode is commoncorruption, --commoncorruption and --severity must be specified")
 
 if args.scenario == "attack" and args.attack_type is None:
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+    raise ValueError("If --scenario is attack, --attack must be specified")
+=======
+>>>>>>> Stashed changes
     raise ValueError("If --mode is attack, --attack must be specified")
 
 # Add the CFN#
 cfnet_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'CFNet'))
 sys.path.append(cfnet_path)
+<<<<<<< Updated upstream
+=======
+>>>>>>> disparity_estimation/feature/wrapper
+>>>>>>> Stashed changes
 
 # https://mlflow.org/docs/latest/python_api/mlflow.html#mlflow.start_run
 with mlflow.start_run(experiment_id='128987742873377588'):
@@ -29,9 +39,43 @@ with mlflow.start_run(experiment_id='128987742873377588'):
     if args.model.lower() == "cfnet":
         from CFNet import main
 
+<<<<<<< Updated upstream
     else:
         raise ValueError("Architecture not recognized")
 
+    print(f"Running {args.scenario} mode for {args.model}")
+
+
+    run_name = f"{args.model}_{args.scenario}"
+    if args.scenario == "commoncorruption":
+        run_name = f"{args.model}_{args.scenario}_{args.commoncorruption}_{args.severity}"
+    elif args.scenario == "attack":
+        run_name = f"{args.model}_{args.scenario}_{args.attack_type}"
+
+    mlflow.set_tag("mlflow.runName", run_name)
+
+=======
+<<<<<<< HEAD
+    print(f"Running {args.scenario} scenario for {args.architecture}")
+>>>>>>> Stashed changes
+        
+    if args.scenario == "train":
+        main.train()
+
+    elif args.scenario == "test" or args.scenario == "commoncorruption":
+        main.test()
+
+    elif args.scenario == "attack":
+        main.attack(attack_type=args.attack_type)
+
+<<<<<<< Updated upstream
+=======
+        if args.commoncorruption not in args.unknown:
+            raise ValueError("Common corruption not recognized")
+
+        mlflow.set_tag("mlflow.runName", f"{args.scenario}_commoncorruption_{args.commoncorruption}_{args.severity}")
+        main.test()
+=======
     print(f"Running {args.scenario} mode for {args.model}")
 
 
@@ -53,5 +97,7 @@ with mlflow.start_run(experiment_id='128987742873377588'):
     elif args.scenario == "attack":
         main.attack(attack_type=args.attack_type)
 
+>>>>>>> disparity_estimation/feature/wrapper
+>>>>>>> Stashed changes
     else:
         raise ValueError("Mode not recognized")
