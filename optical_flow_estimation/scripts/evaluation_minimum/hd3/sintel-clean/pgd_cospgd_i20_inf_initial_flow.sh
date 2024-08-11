2c31ae2f5b9aa81125458a3dfe931d75181afa70
LucaLuca1124 <schwarz.luca1@gmx.de>
2024-08-11 19:20:21 +0200

    feat: new experiment scripts for optimization w.r.t. initial flow

---
+++ optical_flow_estimation/scripts/evaluation_minimum/hd3/sintel-clean/pgd_cospgd_i20_inf_initial_flow.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=100G
+#SBATCH --time=04:59:59
+#SBATCH --cpus-per-task=16
+#SBATCH --gres=gpu:1
+#SBATCH --partition=gpu_4
+#SBATCH --array=0-1%2
+#SBATCH --job-name=hd3_sintel-clean_pgd_cospgd_i20_inf_init
+#SBATCH --output=slurm/hd3_sintel-clean_pgd_cospgd_i20_inf_init_%A_%a.out
+#SBATCH --error=slurm/hd3_sintel-clean_pgd_cospgd_i20_inf_init_err_%A_%a.out
+
+model="hd3"
+dataset="sintel-clean"
+checkpoint="sintel"
+targeted="False"
+norm="inf"
+attacks="pgd cospgd"
+iterations="20"
+jobnum=0
+#SLURM_ARRAY_TASK_ID=0
+
+cd ../../../../
+
+epsilons="8"
+alphas="0.01"
+
+for epsilon in $epsilons
+do
+    epsilon=$(echo "scale=10; $epsilon/255" | bc)
+    for alpha in $alphas
+    do
+        for attack in $attacks
+        do
+            for iteration in $iterations
+            do                         
+                if [[ $SLURM_ARRAY_TASK_ID -eq $jobnum ]]
+                then
+                    echo "Running job $model $checkpoint $dataset $attack $iteration $norm $alpha $epsilon $targeted $jobnum"
+                    python attacks.py \
+                        $model \
+                        --pretrained_ckpt $checkpoint \
+                        --val_dataset $dataset \
+                        --attack $attack \
+                        --attack_iterations $iteration \
+                        --attack_norm $norm \
+                        --attack_alpha $alpha \
+                        --attack_epsilon $epsilon \
+                        --attack_targeted $targeted \
+                        --attack_optim_target "initial_flow"
+                        --write_outputs 
+                    #SLURM_ARRAY_TASK_ID=$((SLURM_ARRAY_TASK_ID + 1))
+                fi
+                jobnum=$((jobnum + 1))
+            done
+        done
+    done
+done
