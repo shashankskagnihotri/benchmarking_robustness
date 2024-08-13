e56987bf4765d5c824ae74615736f0e820b69d66
roesch01 <fabi.roesch01@gmail.com>
2024-08-13 13:57:13 +0200

    Checked whether common corruptions are made for all kitti images

--- disparity_estimation/common_corruptions/create_common_corruptions_kitti.py
+++ disparity_estimation/common_corruptions/create_common_corruptions_kitti.py
@@ -3,7 +3,7 @@ from imagecorruptions import get_corruption_names, corrupt
 from PIL import Image
 import numpy as np
 import concurrent.futures
-from dataloader import get_dataset
+from dataloader import get_data_loader_1
 import logging
 
 
@@ -26,6 +26,7 @@ logger.info('Begin of corruption')
 
 # KITTI Dataset
 def process_batch(batch_indices, dataloader, corruption_names):
+    print(type(dataloader))
     for i in batch_indices:
         image_left_path = dataloader.left_data[i]
         image_right_path = dataloader.right_data[i]
@@ -77,12 +78,45 @@ def parallel_process(dataloader):
         for future in concurrent.futures.as_completed(futures):
             future.result()  # Rufe result auf, um mögliche Ausnahmen zu erfassen
 
-# get dataset
-dataloader = get_dataset('kitti','/pfs/work7/workspace/scratch/ma_faroesch-team_project_fss2024/dataset/KITTI_2015','test','')
 
+class Args:
+    def __init__(self):
+        self.dataset = 'kitti'
+        self.datapath = '/pfs/work7/workspace/scratch/ma_faroesch-team_project_fss2024/dataset/KITTI_2015/Common_corruptions/brightness/severity_4'
+        self.architecture_name = ''
+        self.split = 'train'
+        self.batch_size = 1
+        self.test_batch_size = 1
+
+
+# get dataset
+TrainImgLoader, ValImgLoader, TestImgLoader = get_data_loader_1(Args(), 'gwcnet')
 # parallel_process(dataloader)
 
+print("TrainImgLoader", len(TrainImgLoader.dataset))
+print("ValImgLoader", len(ValImgLoader.dataset))
+print("TestImgLoader", len(TestImgLoader.dataset))
+
+# Get the filenames of each image in the dataset
+train_filenames = [os.path.basename(img_path) for img_path in TrainImgLoader.dataset.dataset.left_data]
+val_filenames = [os.path.basename(img_path) for img_path in ValImgLoader.dataset.dataset.left_data]
+test_filenames = [os.path.basename(img_path) for img_path in TestImgLoader.dataset.dataset.left_data]
+
+# Compare if the filenames are equal
+if train_filenames == val_filenames and val_filenames == test_filenames:
+    print("All dataset filenames are equal")
+else:
+    print("Dataset filenames are not equal")
+
+print(train_filenames[:-3])
+print(val_filenames[:-3])
+print(test_filenames[:-3])
+
+exit()
+
 # Läuft rückwarts
-process_batch(range(len(dataloader) - 1, -1, -1), dataloader, get_corruption_names())
+process_batch(range(len(TrainImgLoader.dataset) - 1, -1, -1), TrainImgLoader.dataset, get_corruption_names())
+process_batch(range(len(ValImgLoader.dataset) - 1, -1, -1), ValImgLoader.dataset, get_corruption_names())
+process_batch(range(len(TestImgLoader.dataset) - 1, -1, -1), TestImgLoader.dataset, get_corruption_names())
 
 logging.shutdown()
