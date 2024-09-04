a0eefc0cbfbf37f461a0dc37a4106e479d1279a8
RunDataExe <run.data.exe@gmail.com>
2024-09-04 14:10:09 +0200

    Started with Test setup for horeka

---
+++ object_detection/horeka_test_submission/atss_cascade_rcnn_convnext_swin-b.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#SBATCH --job-name=test_submission_atss_and_cascade_rcnn_for_convnext_and_swin-b  # job name
+#SBATCH --output=object_detection/slurm/train_results/log.%x.job_%j.out
+#SBATCH --error=object_detection/slurm/train_results/log.%x.job_%j.err
+#SBATCH --time=00:10:00         # maximum wall time allocated for the job (D-H:MM:SS)
+#SBATCH --partition=accelerated # put the job into the gpu partition
+
+
+## Nodes and tasks allocation
+#SBATCH --nodes=1               # number of nodes
+#SBATCH --ntasks-per-node=4     # MPI processes per node (1 task per GPU)
+
+## GPU allocation 
+#SBATCH --gres=gpu:4            # number of GPUs per node
+
+# Load necessary modules
+module load devel/cuda/11.8
+
+# Run each model training task on a separate GPU
+srun --gres=gpu:1 --ntasks=1 --exclusive python tools/train.py \
+    ./object_detection/horeka_test_submission/atss_convnext-b_coco.py \
+    --work-dir ./object_detection/slurm/train_work_dir/atts_convnext-b_coco \
+    --resume \
+    --auto-scale-lr &
+
+srun --gres=gpu:1 --ntasks=1 --exclusive python tools/train.py \
+    ./object_detection/horeka_test_submission/atss_swin-b_coco.py \
+    --work-dir ./object_detection/slurm/train_work_dir/atts_swin-b_coco \
+    --resume \
+    --auto-scale-lr &
+
+srun --gres=gpu:1 --ntasks=1 --exclusive python tools/train.py \
+    ./object_detection/horeka_test_submission/cascade_rcnn_convnext-b_coco.py \
+    --work-dir ./object_detection/slurm/train_work_dir/cascade_rcnn_convnext-b_coco \
+    --resume \
+    --auto-scale-lr &
+
+srun --gres=gpu:1 --ntasks=1 --exclusive python tools/train.py \
+    ./object_detection/horeka_test_submission/cascade_rcnn_swin-b_coco.py \
+    --work-dir ./object_detection/slurm/train_work_dir/cascade_rcnn_swin-b_coco \
+    --resume \
+    --auto-scale-lr &
+
+# Wait for all background jobs to finish
+wait
