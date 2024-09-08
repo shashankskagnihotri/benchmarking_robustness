625de935b45d1eff6ec0ac1eab17d920e66e1fdb
Julian Caspary <julian.yuya.caspary@students.uni-mannheim.de>
2024-09-08 20:20:11 +0200

    3dcc scripts

---
+++ optical_flow_estimation/scripts/evaluation_minimum/matchflow/kitti-2015/3dcc.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=100G
+#SBATCH --time=00:29:59
+#SBATCH --cpus-per-task=16
+#SBATCH --gres=gpu:1
+#SBATCH --partition=gpu_8
+#SBATCH --array=0-7%4
+#SBATCH --job-name=matchflow_kitti-2015_3dcc
+#SBATCH --output=slurm/matchflow_kitti-2015_3dcc_%A_%a.out
+#SBATCH --error=slurm/matchflow_kitti-2015_3dcc_err_%A_%a.out
+
+model="matchflow"
+dataset="kitti-2015"
+checkpoint="kitti"
+attack="3dcc"
+tdcc_corruptions="far_focus near_focus fog_3d color_quant iso_noise low_light xy_motion_blur z_motion_blur"
+tdcc_intensities="3"
+jobnum=0
+
+#SLURM_ARRAY_TASK_ID=0
+
+cd ../../../../
+for tdcc_corruption in $tdcc_corruptions
+do
+    for tdcc_intensity in $tdcc_intensities
+    do
+        if [[ $SLURM_ARRAY_TASK_ID -eq $jobnum ]]
+        then
+            echo "Running job $model $checkpoint $dataset $attack $tdcc_corruption $tdcc_intensity $jobnum"
+            python attacks.py \
+                $model \
+                --pretrained_ckpt $checkpoint \
+                --val_dataset $dataset \
+                --attack $attack \
+                --3dcc_corruption $tdcc_corruption \
+                --3dcc_intensity $tdcc_intensity \
+                --write_outputs                                       
+            #SLURM_ARRAY_TASK_ID=$((SLURM_ARRAY_TASK_ID + 1))
+        fi
+        jobnum=$((jobnum + 1))
+    done
+done
